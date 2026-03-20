ca7631aaf0c1c39c0cf4795d8c19a762fe9238be
Ganesh-Jana <janaganesh810@gmail.com>
2026-03-21 03:50:48 +0530

    fix: update CORS configuration to use HTTPS for frontend URL

--- src/main/java/com/btechhub/config/SecurityConfig.java
+++ src/main/java/com/btechhub/config/SecurityConfig.java
@@ -53,8 +53,10 @@ public class SecurityConfig {
     @Bean
     public CorsConfigurationSource corsConfigurationSource() {
         CorsConfiguration config = new CorsConfiguration();
-        config.setAllowedOrigins(List.of("http://localhost:3000",
-                "b-tech-hub-frontend.vercel.app")); // React dev server
+        config.setAllowedOrigins(List.of(
+                "http://localhost:3000",
+                "https://b-tech-hub-frontend.vercel.app"));
+        // React dev server
         config.setAllowedMethods(List.of("GET", "POST", "PUT", "PATCH", "DELETE", "OPTIONS"));
         config.setAllowedHeaders(List.of("*"));
         config.setAllowCredentials(true);
