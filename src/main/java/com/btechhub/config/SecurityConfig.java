1d1a3459ea3a695baa0182361b2273b597698219
Ganesh-Jana <janaganesh810@gmail.com>
2026-03-21 02:15:02 +0530

    fix: update CORS configuration to allow all origins

--- src/main/java/com/btechhub/config/SecurityConfig.java
+++ src/main/java/com/btechhub/config/SecurityConfig.java
@@ -53,7 +53,7 @@ public class SecurityConfig {
     @Bean
     public CorsConfigurationSource corsConfigurationSource() {
         CorsConfiguration config = new CorsConfiguration();
-        config.setAllowedOrigins(List.of("http://localhost:3000")); // React dev server
+        config.setAllowedOrigins(List.of("*")); // React dev server
         config.setAllowedMethods(List.of("GET", "POST", "PUT", "PATCH", "DELETE", "OPTIONS"));
         config.setAllowedHeaders(List.of("*"));
         config.setAllowCredentials(true);
